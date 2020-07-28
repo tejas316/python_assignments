e2ec5e5ff79d32180c613c0a2527d9fbed6b40dc
rutuja-sudrik <68853152+rutuja-sudrik@users.noreply.github.com>
2020-07-28 19:20:35 +0530

    Add files via upload

---
+++ python_assignments/Assignment2/List1_rutuja.py
@@ -0,0 +1,62 @@
+def match_ends(words):
+    count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            count += 1
+    return count
+
+
+def front_x(words):
+    list_1 = []
+    list_2 = []
+    for word in words:
+        if word[0] == 'x':
+            list_1.append(word)
+        else:
+            list_2.append(word)
+    return sorted(list_1) + sorted(list_2)
+
+
+def last_element(tuples):
+    return tuples[-1]
+
+
+def sort_last(tuples):
+    return sorted(tuples, key=last_element)
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+def main():
+    print 'match_ends'
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print
+    print 'front_x'
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print
+    print 'sort_last'
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+    main()
